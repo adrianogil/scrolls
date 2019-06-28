eb749e9ad03089b434bfbe17dc5cf2faa47b6ea8
Adriano Gil <adrianogil.san@gmail.com>
2019-06-28 00:09:43 -0400

    Implemented simple commands cd and ls

--- src/python/scrolls/terminal_client.py
+++ src/python/scrolls/terminal_client.py
@@ -7,11 +7,18 @@ PORT = 9000
 DATA = 'AAAAAAAAAA'
 
 
-def udp_client(command_to_send):
+def udp_client(command_to_send, answer_handler_callback=None):
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client.sendto(command_to_send.encode(), (HOST, PORT))
     data, addr = client.recvfrom(4096)
-    print("%s %s" % (data, addr))
+    # print("%s %s\n" % (data, addr))
+    if answer_handler_callback is not None:
+        answer_handler_callback(data)
+
+
+def show_commands_output(cmd_output):
+    cmd_output = cmd_output.decode("utf8")
+    print(cmd_output)
 
 
 if __name__ == '__main__':
@@ -19,4 +26,4 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         command_to_send = sys.argv[1]
 
-    udp_client(command_to_send)
+    udp_client(command_to_send, show_commands_output)
--- src/python/scrolls/terminal_server.py
+++ src/python/scrolls/terminal_server.py
@@ -1,18 +1,51 @@
+import os
 import socket
 
 BIND_IP = '0.0.0.0'
 BIND_PORT = 9000
 
-def udp_server():
+
+def udp_server(data_handler_callback=None):
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server.bind(( BIND_IP, BIND_PORT))
+    server.bind((BIND_IP, BIND_PORT))
     print("Waiting on port: " + str(BIND_PORT))
 
     while 1:
         data, addr = server.recvfrom(1024)
         print(addr)
-        print(data)
-        server.sendto('ACK'.encode(), addr)
+        answer = 'ACK'
+        if data_handler_callback is not None:
+            answer = data_handler_callback(data)
+
+        server.sendto(answer.encode(), addr)
+
+
+def command_handler(command_received):
+    command_received = command_received.decode("utf8")
+    print("Command received: %s" % (command_received,))
+    answer = 'ACK'
+    if command_received == "ls":
+        answer = ""
+        dir_content = os.listdir()
+        for content in dir_content:
+            answer += content + "\n"
+    elif command_received[0:2] == "cd":
+        next_path = ""
+        if len(command_received) == 2:
+            home_path = os.path.expanduser("~")
+            os.chdir(home_path)
+            next_path = home_path
+        else:
+            cmd_split = command_received.split(" ")
+            target_path = cmd_split[1]
+            os.chdir(target_path)
+            next_path = target_path
+        next_path = os.path.abspath(next_path)
+        answer = "Moved to path: %s" % (next_path,)
+
+    return answer
+
+
 
 if __name__ == '__main__':
-    udp_server()
+    udp_server(command_handler)
